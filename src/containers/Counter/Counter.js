36c649afdd9e0a5c69fbee496a1ac3d60c672e87
Ahmad Fraidoon Faqiri <affaqiri@outlook.com>
2019-01-31 21:36:22 +1100

    Step 3: mapDisptachToProps for registering component actions with the store

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -32,13 +32,22 @@ class Counter extends Component {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
             </div>
         );
     }
+}
+
+const mapDisptachToProps = dispatch => {
+    // dispatch is a redux function passed as an argument and it will call the dispatch function of redux on the store
+    // return an object that will hold some properties which hold reference to some functions that will dispatch some actions
+    return {
+        //mapping of prop property to an anonymous function
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+    };
 }
 
 const mapStateToProps = state => {
@@ -52,4 +61,8 @@ const mapStateToProps = state => {
 // connect is a higher order component 
 // we need to specify which part (slice) of the global application state is relevant to our component
 // and which actions we want to dispatch from our component
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDisptachToProps)(Counter);
+
+// different variations
+//export default connect(mapStateToProps)(Counter);
+//export default connect(null, mapDisptachToProps)(Counter);
