3661512cfc1b059c8204f5d5fcdeccc70c377430
Ahmad Fraidoon Faqiri <affaqiri@outlook.com>
2019-02-01 00:12:57 +1100

    Step 9: adding enhancers or middlewares

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
@@ -16,7 +16,20 @@ const rootReducer = combineReducers({
     res: resultReducer
 })
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching]', action);
+            // here we can change the action before passing it to the reducer
+            // but we need to be careful with what we do to the state
+            const result = next(action);   
+            console.log('[Middleware] next state]', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));    // the second argument is an enhancer aka middleware
 
 // Provider is a helper component that helps us inject the store to react components
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
