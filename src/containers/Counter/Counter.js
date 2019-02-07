3ce8bed17e8a86ba37b245511a1b7589f623d4a3
Ahmad Fraidoon Faqiri <affaqiri@outlook.com>
2019-02-08 00:11:45 +1100

    Step 13: reorganizing action types and action creators

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -4,8 +4,7 @@ import { connect } from "react-redux";
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
-//import * as actionTypes from '../../store/actions/actions';
-import * as actionCreators from '../../store/actions/actions';
+import * as actionCreators from '../../store/actions/index';
 
 // Container type of component
 class Counter extends Component {
--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,39 +1,8 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const STORE_RESULT = 'STORE_RESULT';
-export const DELETE_RESULT = 'DELETE_RESULT';
-
-export const increment = () => {
-  return {
-    type: INCREMENT
-  };
-}
-
-export const decrement = () => {
-  return {
-    type: DECREMENT
-  };
-}
-
-export const add = value => {
-  return {
-    type: ADD,
-    value: value
-  };
-}
-
-export const subtract = value => {
-  return {
-    type: SUBTRACT,
-    value: value
-  };
-}
+import * as actionTypes from "./actionTypes";
 
 export const storeResult = res => {
   return {
-    type: STORE_RESULT,
+    type: actionTypes.STORE_RESULT,
     result: res
   };
 }
@@ -52,7 +21,7 @@ export const storeResultAsync = res => {
 
 export const deleteResult = resEltId => {
   return {
-    type: DELETE_RESULT,
+    type: actionTypes.DELETE_RESULT,
     resultElementId: resEltId
   };
 }
